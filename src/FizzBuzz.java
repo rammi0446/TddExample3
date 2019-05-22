d098174bef10c12b21b338d5c620f72574eeccd2
Navjot Gill <Navjot Gill@Raman>
2019-05-22 13:05:12 -0400

    R5- Green Prime

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,29 @@
 
 public class FizzBuzz {
+	public boolean checkPrime(int num)
+	{
+		//check number is prime
+		boolean isPrime = true;
+		for(int i =2; i< num-1; i++)
+		{
+			if(num%i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		System.out.println("num :" +num);
+		System.out.println("is prime :" +isPrime);
+		return isPrime;
+	}
 public String buzzzz(int num)
 {
+boolean isPrime = checkPrime(num);
+	//
+	if(isPrime == true)
+	{
+		return "whizz";
+	}
 	if(num%15 == 0)
 	{
 	return "fizzbuzz";

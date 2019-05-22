790849c2ae180f2f42302a7012569876c6f4535a
Navjot Gill <Navjot Gill@Raman>
2019-05-22 12:24:14 -0400

    R3-Green

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,14 +2,19 @@
 public class FizzBuzz {
 public String buzzzz(int num)
 {
-	if(num%3 == 0)
+	if(num%15 == 0)
 	{
-	return "fizz";
+	return "fizzbuzz";
 	}
 	else if(num %5 == 0)
 	{
 		return "buzz";
 	}
+	else if(num % 3 == 0)
+	{
+		return "fizz";
+	}
+	
 	return null;
 }
 }
